cd562750c0dfd54e2508ca2ea11e6a34903247a3
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-07-06 03:54:23 +0000

    creat a new timer

---
+++ timer2.js
@@ -0,0 +1,24 @@
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+
+console.log("Enter number from 1 to 9 to set a timer:");
+stdin.on('data', (key) => {
+
+  if (key === 'b') {
+    process.stdout.write('\x07');
+  }
+
+  if (key >= 1 && key <= 9) {
+    console.log(`setting timer for ${key} seconds`);
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, key * 1000);
+
+  }
+
+  if (key === '\u0003') {
+    console.log('Thanks for using me, ciao!');
+    process.exit();
+  }
+});
